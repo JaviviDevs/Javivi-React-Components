c73e74661e08a757e876a053bf6bfde09abafc56
JaviviDevs <javierexmar@gmail.com>
2023-10-31 01:42:44 +0100

    Se ha creado un nuevo componente, es un card con borde izquierdo visible y un circulo central

---
+++ left-circle-card/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { LeftCircleCard } from './left-circle-card/leftCircleCard.jsx';
+import './App.css'
+
+
+export function App() {
+  return (
+    <>
+    <LeftCircleCard company='GranaSat' work='Software Developer (10/2022-07/2023)'/>
+    <LeftCircleCard company='GranaSat' work='Software Developer (10/2022-07/2023)'/>
+    <LeftCircleCard company='GranaSat' work='Software Developer (10/2022-07/2023)'/>
+    </>
+    
+  )
+}
